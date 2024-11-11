4085607433680ca1961c16ee5d4c82cad9571508
deshydf <155445472+yash-deshpande24@users.noreply.github.com>
2024-11-11 13:14:01 +0000

    variables updated

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 145978
+let accountEmail = "jgfyt@gmail.com"
+var accountpassword = "123446"
+accountcity = "pune"
+
+//accountId = 146668 
+accountEmail = "gfghg@gmail.com"
+accountpassword ="466890"
+accountcity = "nanded"
+
+console.log(accountId);
+
+/*
+prefer not use for var
+because issues is blocks in block scope & function scope
+*/
+console.table([accountId,accountEmail,accountpassword,accountcity])
